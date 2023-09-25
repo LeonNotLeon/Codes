9ff1755e5c4f2984480f2dbcab11f8d1552ae524
Leon Wu <leon.yuangwu@gmail.com>
2023-09-25 20:11:01 +1300

    Python

---
+++ Python1.py
@@ -0,0 +1,18 @@
+#print("hellow world!")
+#i = 1
+#sum = 0
+#while i <= 100:
+#    total = sum + i
+#    print(total)
+#    sum = total
+#    i+=1
+
+i = 1
+sum = 0
+print(sum)
+print(i)
+while i <= 34:
+    total = i + sum
+    print(i)
+    sum = i
+    i += total
