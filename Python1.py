c6acb886e9c7c43b89e0f3bec36b205053ea2db1
Leon Wu <leon.yuangwu@gmail.com>
2023-09-26 15:39:06 +1300

    Changes to be committed: 	modified:   Python1.py

--- Python1.py
+++ Python1.py
@@ -7,13 +7,27 @@
 #    sum = total
 #    i+=1
 
+#i = 1
+#j = 0
+#print(j)
+#print(i)
+#while i <= 34:
+#    total = i + j
+#   print(total)
+#    j = i
+#    i = total
+
 i = 1
+j = 0
 sum = 0
-print(sum)
+sum = i + j
+print(j)
 print(i)
-while i <= 34:
-    total = i + sum
-    print(i)
-    sum = i
-    i += total
-    #sdjflsdjklfsldjfklsdjfksdjf
+while i <= 21:
+    total = j + i
+    print(total)
+    sum += total
+    j = i
+    i = total
+
+print("#" + str(sum))
