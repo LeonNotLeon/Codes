35db51e073cc5e8ecbae95eb056f4f5f6dc96485
Leon Wu <leon.yuangwu@gmail.com>
2023-09-29 15:15:13 +1300

    TrigFunctions

---
+++ PythonTrig.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import math
+
+print ("##########################################\n")
+
+print (math.degrees(math.acos(1/2)))
+print (math.degrees(math.asin(1/2)))
+print (math.degrees(math.atan(1)))
+print (math.atan2(1, 2))
+print (math.cos(math.pi/3))
+print (math.hypot(5, 12))
+print (math.sin(math.pi/6))
+print (math.tan(math.pi/4))
+print (math.degrees(2*math.pi))
+print (math.radians(180))
+print (math.pi)
+print (math.e)
+
+print ("\n##########################################")
